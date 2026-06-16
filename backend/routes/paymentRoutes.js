42ece56a0ee13587d3db8aa4ae67fce82a76f52c
kartik <kartikpatilxyz5@gmail.com>
2026-06-16 11:52:30 +0530

    add payment route test

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,13 +1,6 @@
-const express = require("express");
-const router = express.Router();
-
-const {
-  createOrder,
-  webhook
-} = require("../controllers/paymentController");
-
-router.post("/create-order", createOrder);
-
-router.post("/cashfree/webhook", webhook);
-
-module.exports = router;
+router.get("/test", (req, res) => {
+  res.json({
+    success: true,
+    route: "payment route working"
+  });
+});
